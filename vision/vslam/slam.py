Fix incorrect position and orientation calculation and correlation

vision/vslam/slam.py
@@ -1,11 +1,12 @@
 import math
 from typing import List, Tuple
 import numpy as np
+import scipy
 
 from vslam.state import Delta
 from vslam.utils import angle_axis, find_orthogonal_axes
 from vslam.state import ControlState, Delta, State
-from vslam.database import Feature, feature_database
+from vslam.database import Feature, Observe, ProcessedFeatures, feature_database
 
 class SLAM:
   """
@@ -31,7 +32,17 @@ class SLAM:
 
     # Apply Gradient Ascent on visual measurement probability by computing derivatives
     # using the fundamental theorem of calculus
+    measurements, pr = feature_database.observe(estimate, frame, Observe.VISUAL_MEASUREMENT)
+    if len(measurements) == 0:
+      return Delta(), 0.0
+    measurements.sort(key=lambda x: x.importance_weight)
+    softmax = scipy.special.softmax(list(map(lambda x: x.importance_weight, measurements)))
     delta = Delta()
+    for i, measurement in enumerate(measurements):
+      delta.delta_position += softmax[i] * measurement.delta.delta_position
+      delta.delta_theta += softmax[i] * measurement.delta.delta_theta
+      delta.delta_phi += softmax[i] * measurement.delta.delta_phi
+    return delta, pr
     _, last_probability = feature_database.observe(estimate, frame, False)
     print(last_probability)
     if last_probability == 0.0:
@@ -42,7 +53,7 @@ class SLAM:
         break
       position_gradient = []
       for axis in SLAM.BASIS:
-        positive = Delta(delta.delta_position + axis * RESOLUTION_POSITION, delta.delta_orientation)
+        positive = Delta(delta.delta_position + axis * RESOLUTION_POSITION, delta.delta_theta, delta.delta_phi)
         _, p_upper = feature_database.observe(estimate.apply_delta(positive), frame, False)
         p_lower = last_probability
         position_gradient.append((p_upper - p_lower) / RESOLUTION_POSITION)
@@ -56,7 +67,7 @@ class SLAM:
         orientation_gradient.append((p_upper - p_lower) / RESOLUTION_ANGLE)
       next = Delta()
       next.delta_position += LR * np.asarray(position_gradient)
-      orientation = estimate.forward + delta.delta_orientation
+      orientation = estimate.forward + delta.delta_orientation # recompute reference frame
       orientation = np.dot(angle_axis(rotation_axes[0], LR * orientation_gradient[0]), orientation)
       orientation = np.dot(angle_axis(rotation_axes[1], LR * orientation_gradient[1]), orientation)
       next.delta_orientation = orientation - estimate.forward

vision/vslam/state.py
@@ -4,17 +4,19 @@ from typing import Any, List, Tuple
 from dataclasses import dataclass
 import numpy as np
 
-from vslam.utils import Position, Vector, angle_axis, normalize
+from vslam.utils import Z_AXIS, Position, Vector, angle_axis, normalize, spherical_rotation_matrix
 
 @dataclass
 class Delta:
   delta_position: Vector = np.asarray((0.0, 0.0, 0.0))
-  delta_orientation: Vector = np.asarray((0.0, 0.0, 0.0))
+  delta_theta: float = 0.0
+  delta_phi: float = 0.0
   
   def negate(self) -> 'Delta':
     return Delta(
       -self.delta_position,
-      -self.delta_orientation
+      -self.delta_theta,
+      -self.delta_phi
     )
   
 @dataclass
@@ -26,10 +28,11 @@ class State:
   orientation_deviation: float = 0.0
   
   def apply_delta(self, delta: Delta) -> 'State':
+    rotation = spherical_rotation_matrix(delta.delta_theta, delta.delta_phi)
     return State(
       position=self.position + delta.delta_position,
-      forward=normalize(self.forward + delta.delta_orientation),
-      up=normalize(self.up + delta.delta_orientation),
+      forward=normalize(np.dot(rotation, self.forward)),
+      up=normalize(np.dot(rotation, self.up)),
       position_deviation=self.position_deviation,
       orientation_deviation=self.orientation_deviation
     )