010567b5d46558de5bba4c8cd416a73893fd3b1d
Patrick Stanislaw Hadlaw <pshadlaw@uwaterloo.ca>
2022-03-08 16:48:37 -0500

    Implement client and map updating

--- vision/main.py
+++ vision/main.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import matplotlib.image as mpimg
 
 from vslam.app import App
 from vslam.config import CONFIG, DebugWindows
@@ -9,16 +8,12 @@ def main():
   parser = argparse.ArgumentParser(description='VSLAM runner.')
   parser.add_argument('-d', '--debug', dest='debug', type=str)
   parser.add_argument('-c', '--clear', dest='clear', action='store_true')
-  parser.add_argument('-v', '--visualize', dest='visualize', action='store_true')
   parser.add_argument('-q', '--quit', dest='quit', action='store_true')
   args = parser.parse_args()
 
   CONFIG.windows = DebugWindows.parse(args.debug)
 
-  app = App()
-
-  if args.visualize:
-    mpimg.imsave(os.path.join(CONFIG.databasePath, 'map.png'), app.visualize())
+  app = App()    
 
   if args.clear:
     app.clear()
